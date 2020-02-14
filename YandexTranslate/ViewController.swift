3e61b1a88019c9387b38a794283ec3cae4303007
Nikolay Lukyanchikov <kalyastiy@gmail.com>
2020-02-14 13:55:52 +0300

    Initial Commit

---
+++ YandexTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YandexTranslate
+//
+//  Created by Nikolay Lukyanchikov on 14.02.2020.
+//  Copyright © 2020 Nikolay Lukyanchikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
